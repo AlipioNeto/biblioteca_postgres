b02f2f712c9913795a78228d4db7dc5ef1b5f1e4
Alipio <alipio.oliveira@om30.com.br>
2026-04-30 08:58:05 -0300

    cancela_requisicao_consumo

---
+++ consultas/cancelar_requisicao_consumo.sql
@@ -0,0 +1,19 @@
+-- =========================================
+-- Nome: Cancelamento requisicao estoque_requisicoes_consumos
+-- Categoria: consultas
+-- Tags: cancela_requisicao_consumo
+-- Cliente: Cotia 
+-- requisicao numero : 116019
+-- =========================================
+
+-- https://cotia.saudesimples.net/estoque/requisicoes_consumos
+
+select id,situacao_requisicao_consumo_id from estoque_requisicoes_consumos erc where codigo ilike '%116019%'  
+-- situacao = 3 (separada)
+--          = 5 (cancelada)
+
+UPDATE public.estoque_requisicoes_consumos SET situacao_requisicao_consumo_id = 5 WHERE id=38169;
+
+select *from estoque_situacoes_requisicoes_consumos esrc 
+
+
